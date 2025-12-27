f4fefb8bc4a48af84323f470df52002da38e041e
pawanshewale69-web <pawanshewale69@gmail.com>
2025-12-27 22:56:09 +0530

    Implement FAISS vector DB and embeddings

---
+++ Context-Aware-Semantic-File-System-main/backend/embeddings/embedder.py
@@ -0,0 +1,10 @@
+from sentence_transformers import SentenceTransformer
+
+# Load lightweight BERT-based model
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
+def get_embedding(text):
+    """
+    Convert input text into numerical vector (embedding)
+    """
+    return model.encode(text)
